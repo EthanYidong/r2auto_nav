3f66c148fd750d24bb1f4ad9a48025e448f9f093
Ethan Tang <ethanyidong@gmail.com>
2022-03-31 13:11:10 +0800

    Added servo control code

--- ts_client/ts_client/motors.py
+++ ts_client/ts_client/motors.py
@@ -20,18 +20,25 @@ class Motors(Node):
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(22, GPIO.OUT)
 
         self.flywheels = GPIO.PWM(18, 1000)
+        self.servo = GPIO.PWM(18, 50)
+        self.servo.ChangeDutyCycle(7.5)
         self.get_logger().info("Motor runner started")
 
     def motor_callback(self, msg):
         cmd = msg.data
-        if(cmd == 1):
+        if(cmd == 0):
+            self.flywheels.start(0)
+        elif(cmd == 1):
             for i in range(101):
                 self.flywheels.start(i)
                 time.sleep(0.02)
-        elif(cmd == 0):
-            self.flywheels.start(0)
+        elif(cmd == 2):
+            self.servo.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+            self.servo.ChangeDutyCycle(7.5)
         pass
                 
 def main(args=None):
