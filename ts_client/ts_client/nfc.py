62382d91a4f09250d3649ac38ff5bfd9d2074716
EthanYidong <50757300+EthanYidong@users.noreply.github.com>
2022-04-21 17:01:09 +0800

    Document nfc.py

--- ts_client/ts_client/nfc.py
+++ ts_client/ts_client/nfc.py
@@ -16,6 +16,7 @@ class NFC(Node):
         self.publisher_ = self.create_publisher(Empty, 'nfc', 10)
         self.timer = self.create_timer(PERIOD, self.timer_callback)
 
+	# Initialize PN532 using I2C connection
         self.pn532 = PN532_I2C(debug=False, reset=20, req=16)
         ic, ver, rev, support = self.pn532.get_firmware_version()
         self.get_logger().info('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
@@ -25,6 +26,7 @@ class NFC(Node):
 
     def timer_callback(self):
         try:
+	    # If any uid is returned, that means a card has been found. Report to the server by publishing a message.
             uid = self.pn532.read_passive_target(timeout=0.1)
             if uid is not None:
                 self.get_logger().info("NFC Detected")
