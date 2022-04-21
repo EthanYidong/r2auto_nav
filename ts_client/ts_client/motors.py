711f91fccd2abd4c866f740c920c43da76455d76
EthanYidong <50757300+EthanYidong@users.noreply.github.com>
2022-04-21 16:59:23 +0800

    Document motors.py

--- ts_client/ts_client/motors.py
+++ ts_client/ts_client/motors.py
@@ -18,12 +18,13 @@ class Motors(Node):
             'motor',
             self.motor_callback,
             qos_profile_sensor_data)
-
         self.pi = pigpio.pi()
 
         GPIO.setmode(GPIO.BOARD)
-
+        # Use pigpio for flywheel motors to utilize hardware PWM
         self.pi.set_mode(18, pigpio.OUTPUT)
+        
+        # Use RPi.GPIO for servo motor
         GPIO.setup(22, GPIO.OUT)
 
         self.servo = GPIO.PWM(22, 50)
@@ -35,13 +36,20 @@ class Motors(Node):
 
     def motor_callback(self, msg):
         cmd = msg.data
+        # Use numbers as an enum rather than creating custom message through a package.
+        # 0 = Stop flywheels
+        # 1 = Start flywheels
+        # 2 = Run servo once
         if(cmd == 0):
+            # Set duty cycle to 0
             self.pi.hardware_PWM(18, 10000, 0)
         elif(cmd == 1):
+            # Slowly increase duty cycle to prevent surge current
             for i in range(100):
                 self.pi.hardware_PWM(18, 10000, 10000 * i)
                 time.sleep(0.02)
         elif(cmd == 2):
+            # Move servo backwards to allow ball to fall into tube, then push forward, and finally reset to blocked position.
             self.servo.ChangeDutyCycle(10.5)
             time.sleep(0.5)
             self.servo.ChangeDutyCycle(5.5)
