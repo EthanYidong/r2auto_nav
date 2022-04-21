7e18f595a744b88a6255bd379759e1ced5a0e15c
EthanYidong <50757300+EthanYidong@users.noreply.github.com>
2022-04-21 16:42:04 +0800

    Document firing.py

--- test_scripts/firing.py
+++ test_scripts/firing.py
@@ -2,21 +2,28 @@ import RPi.GPIO as G
 import pigpio
 import time
 
+# Use pigpio for motors to utilize hardware PWM. This addresses surge current that software PWM may fail to prevent under heavy software load.
 pi = pigpio.pi()
+# Use RPi.GPIO for servo.
+# It is unknown if using two different GPIO control libraries have any conflicts, however we did not experience any issues.
 G.setmode(G.BOARD)
 
 pi.set_mode(18, pigpio.OUTPUT)
+
 G.setup(22, G.OUT)
 servo = G.PWM(22, 50)
 
+# Start at 90%, such that the servo arm blocks the ball from falling into the tube.
 servo.start(7.5)
 
+# Slowly increase the duty cycle to prevent surge current from the motors.
 for i in range(100):
     pi.hardware_PWM(18, 10000, 10000 * i)
     time.sleep(0.01)
 
 print("spun up!")
 
+# On enter, pull the servo arm back before pushing it forward, allowing the ball to drop into the firing tube and subsequently pushing it into the flywheels.
 try:
     while True:
         input()
