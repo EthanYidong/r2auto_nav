a130007564bf9231db85e12dd69b9d3130cfbe5a
EthanYidong <50757300+EthanYidong@users.noreply.github.com>
2022-04-21 16:39:09 +0800

    Document calibrate_thermals.py

--- test_scripts/calibrate_thermals.py
+++ test_scripts/calibrate_thermals.py
@@ -6,13 +6,18 @@ import adafruit_mlx90640
 import numpy as np
 import math
 
+# How far left and right to scan and consider. Should be kept the same as the main wallfollow.py script.
 THERMAL_ANGLE_BOUNDS = 55.0
+# How far up and down to scan and consider. Should be kept the same as the main wallfollow.py script.
 THERMAL_H_RANGE = range(8, 16)
+# How wide does the detection have to be before the target is considered found? Should be kept the same as the main wallfollow.py script.
 SAMPLE_THRESHOLD = 5
 
+# Which percentiles of temperature to report values for
 test_percentiles = [50, 60, 70, 80]
 
 
+# Connect to sensor
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C")
@@ -23,6 +28,7 @@ frame = [0] * 768
 target = []
 no_target = []
 
+# Loop through and scan many different non-target locations at the user's discretion, store the temperature values
 while True:
     print("Point at empty space, or target behind a wall, and press enter, or q to continue to targe")
     inp = input()
@@ -33,6 +39,7 @@ while True:
     no_target.append(thermal)
 
 
+# For each possible percentile, find the "minimum threshold" such that all xth percentile temperatures are under this threshold.
 min_threshold = [math.inf for _ in test_percentiles]
 for t in no_target:
     for x in range(32):
@@ -45,6 +52,7 @@ for t in no_target:
 for i, perc in enumerate(test_percentiles):
     print(f"Minimum threshold for {perc} percentile is {min_threshold[i]}")
 
+# Similarly, loop through and scan many different target locations at the user's discretion, store the temperature values
 while True:
     print("Point at target and press enter, or q to end")
     inp = input()
@@ -56,6 +64,7 @@ while True:
 
 max_threshold = [math.inf for _ in test_percentiles]
 
+# For each possible percentile, find the "maximum threshold" such that using that threshold, the xth percentile heat is at least SAMPLE_THRESHOLD wide.
 for t in target:
     sample = [[] for _ in test_percentiles]
     for x in range(32):
